e07f56cccedb275e3f0fedf0c51204b2d3968193
goddns12312-lab <goddns12312@gmail.com>
2026-06-02 06:02:44 +0900

    cleanup: 177개의 불필요한 스크립트 파일 삭제

--- scripts/decode-post-url.js
+++
@@ -1,9 +0,0 @@
-const url = 'LzQwLz9xPVlUb3hPbnR6T2pFeU9pSnJaWGwzYjNKa1gzUjVjR1VpTzNNNk16b2lZV3hzSWp0OSZwYWdlPTg%3D';
-
-// URL 디코드
-const decoded = decodeURIComponent(url);
-console.log('URL 디코드:', decoded);
-
-// Base64 디코드
-const base64Decoded = Buffer.from(decoded, 'base64').toString('utf-8');
-console.log('Base64 디코드:', base64Decoded);
